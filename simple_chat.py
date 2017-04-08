453681ef80614885d004b125c56ec975ca4f377a
MaxOvcharov <ovcharovmax@yandex.ru>
2017-04-08 11:00:09 +0300

    Added argparser parametrs

--- simple_chat.py
+++ simple_chat.py
@@ -1,3 +1,4 @@
+import argparse
 import logging
 import socketio
 
@@ -15,6 +16,10 @@ ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
 ch.setFormatter(f)
 logger.addHandler(ch)
+
+parser = argparse.ArgumentParser(description="aiohttp server example")
+parser.add_argument('--path')
+parser.add_argument('--port')
 
 async def background_task():
     """Example of how to send server generated events to clients."""
@@ -92,7 +97,8 @@ app.router.add_get('/', index)
 
 
 if __name__ == '__main__':
+    args = parser.parse_args()
     sio.start_background_task(background_task)
-    web.run_app(app)
+    web.run_app(app, path=args.path, port=args.port)
 
 
