58e24048489b2c3aee04771598b746926326a3a8
MaxOvcharov <ovcharovmax@yandex.ru>
2017-06-10 23:22:49 +0300

    Added static for login and sign pages

---
+++ chat/static/js/main.js
@@ -0,0 +1,64 @@
+$(document).ready(function(){
+    try{
+        var sock = new WebSocket('ws://' + window.location.host + '/ws');
+    }
+    catch(err){
+        var sock = new WebSocket('wss://' + window.location.host + '/ws');
+    }
+
+    // show message in div#subscribe
+    function showMessage(message) {
+        var messageElem = $('#subscribe'),
+            height = 0,
+            date = new Date();
+            options = {hour12: false};
+        messageElem.append($('<p>').html('[' + date.toLocaleTimeString('en-US', options) + '] ' + message + '\n'));
+        messageElem.find('p').each(function(i, value){
+            height += parseInt($(this).height());
+        });
+
+        messageElem.animate({scrollTop: height});
+    }
+
+    function sendMessage(){
+        var msg = $('#message');
+        sock.send(msg.val());
+        msg.val('').focus();
+    }
+
+    sock.onopen = function(){
+        showMessage('Connection to server started')
+    }
+
+    // send message from form
+    $('#submit').click(function() {
+        sendMessage();
+    });
+
+    $('#message').keyup(function(e){
+        if(e.keyCode == 13){
+            sendMessage();
+        }
+    });
+
+    // income message handler
+    sock.onmessage = function(event) {
+      showMessage(event.data);
+    };
+
+    $('#signout').click(function(){
+        window.location.href = "signout"
+    });
+
+    sock.onclose = function(event){
+        if(event.wasClean){
+            showMessage('Clean connection end')
+        }else{
+            showMessage('Connection broken')
+        }
+    };
+
+    sock.onerror = function(error){
+        showMessage(error);
+    }
+});
