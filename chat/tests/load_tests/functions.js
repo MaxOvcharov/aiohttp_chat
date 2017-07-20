c6ccc4e94c0c0a2bbfe4f88c3fe5d1ca62f697a9
MaxOvcharov <ovcharovmax@yandex.ru>
2017-07-20 17:25:40 +0300

    Added correct scripts for load testing

--- chat/tests/load_tests/functions.js
+++ chat/tests/load_tests/functions.js
@@ -7,23 +7,10 @@
 module.exports = {
   setMessage: setMessage
 };
-
-const MESSAGES = [
-  'what a nice day',
-  'how\'s everybody?',
-  'how\'s it going?',
-  'what a lovely socket.io chatroom',
-  'to be or not to be, that is the question',
-  'Romeo, Romeo! wherefore art thou Romeo?',
-  'now is the winter of our discontent.',
-  'get thee to a nunnery',
-  'a horse! a horse! my kingdom for a horse!'
-];
-
+const uuidv4 = require('uuid/v4');
 function setMessage(context, events, done) {
-  // pick a message randomly
-  const index = Math.floor(Math.random() * MESSAGES.length);
   // make it available to templates as "message"
-  context.vars.message = MESSAGES[index];
+  context.vars.user_id = uuidv4();
+  context.vars.session_id = uuidv4();
   return done();
 }
